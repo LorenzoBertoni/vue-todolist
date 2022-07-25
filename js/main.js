72d3b53f9edb0f283cb2b8b4e8e2fbb4bbbc8886
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-07-25 15:53:38 +0200

    add: metodi per pushare il contenuto dell'input nel DOM all'interno dell'array di cose da fare

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 const app = new Vue({
     el: '#app',
     data: {
-        toDo: '',
-        done: false
+        input: '',
+        toDoList: ['Imparare Vue', 'ripassare js'],
+        newTask: {
+            text: '',
+            done: false
+        },
+    },
+    methods: {
+        addTask() {
+            this.newTask.text = this.input;
+            this.toDoList.push(this.newTask.text);
+            this.input = '';
+        },
+        removeTask(index) {
+            this.toDoList.splice(index, 1);
+        }
     }
 });
