7271108cc41304f27cdc300480eb61f12a49419f
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-07-25 16:36:41 +0200

    add: metodo richiamato al click dei list item che permette di cambiare il valore di 'done' da false a true e di conseguenza. tramite un operatore ternario sempre dei list item, assegnare la text decoration da CSS

--- js/main.js
+++ js/main.js
@@ -2,20 +2,46 @@ const app = new Vue({
     el: '#app',
     data: {
         input: '',
-        toDoList: ['Imparare Vue', 'ripassare js'],
+        toDoList: [
+            {
+                name: 'Imparare Vue',
+                done: false
+            },
+            {
+                name: 'Imparare js',
+                done: false
+            },
+            {
+                name: 'Imparare css',
+                done: false
+            }
+        ],
         newTask: {
             text: '',
-            done: false
         },
     },
     methods: {
         addTask() {
             this.newTask.text = this.input;
-            this.toDoList.push(this.newTask.text);
+            this.toDoList.push(
+                {
+                    name: this.newTask.text,
+                    done: false
+                }
+            );
             this.input = '';
         },
         removeTask(index) {
             this.toDoList.splice(index, 1);
+        },
+        taskDone(index) {
+            if(this.toDoList[index].done == false) {
+                this.toDoList[index].done = true;
+                console.log(this.toDoList[index].done)
+            } else {
+                this.toDoList[index].done = false;
+                console.log(this.toDoList[index].done)
+            }
         }
     }
 });
